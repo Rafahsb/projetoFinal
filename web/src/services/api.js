06e26112aa4067e17f0701c84c8f57b834126138
Rafael <rafaelbona@cge.mt.gov.br>
2023-09-26 18:36:21 -0400

    api aws

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: "http://15.229.74.145:8080",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
