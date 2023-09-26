126dd87b5263838358da590b55d263d49a24b7e8
Rafael Bona <rafaelhbona@gmail.com>
2023-09-26 01:58:28 -0400

    vercel back

--- api/src/server.js
+++ api/src/server.js
@@ -4,14 +4,11 @@ const express = require("express");
 const { routes } = require("./routes");
 const cors = require("cors");
 const server = express();
-const corsOptions = {
-  origin: ['https://viagestaoficdev.netlify.app/', 'http://viagestaoficdev.netlify.app/'],
-};
 
 server.use(express.json());
-server.use(cors(corsOptions));
+server.use(cors());
 server.use(routes);
 
-server.listen("http://15.229.74.145:8080", () => {
+server.listen("http://localhost:8080", () => {
   console.log("Server started at localhost:8080");
 });
