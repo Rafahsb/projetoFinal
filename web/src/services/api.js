beb083dfa2d7b56974671869a66c6aa990895102
Rafael Bona <rafaelhbona@gmail.com>
2023-10-14 18:16:44 -0400

    localhost

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    // baseURL: "http://15.229.74.145:8080",
-    baseURL: "http://52.67.249.100:8080",
+    baseURL: "http://localhost:8080",
     headers: {
         "Content-Type": "application/json",
     },
